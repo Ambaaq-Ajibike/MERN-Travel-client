8bffa94009e9c05f9e101c9c1d42ae50003b2b33
Ambaaq-Ajibike <ajibikeambaaq@gmail.com>
2024-06-08 23:07:44 +0100

    responsiveness and whatsapp

--- src/pages/components/WhatsApp.jsx
+++ src/pages/components/WhatsApp.jsx
@@ -3,9 +3,9 @@ const WhatsAppButton = ({ phoneNumber }) => {
   const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=I want to enquire 200Travel`;
   return (
     <a href={whatsappUrl} target="_blank" rel="noopener noreferrer">
-    <button className="fixed bottom-4 left-4 flex items-center bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded shadow-lg">
-      <FaWhatsapp className="mr-2" />
-      Message on WhatsApp
+    <button className="fixed bottom-4 left-4 flex items-center bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-full shadow-lg">
+      <FaWhatsapp className="mr-2 text-xl" />
+      Message Us
     </button>
   </a>
   );
