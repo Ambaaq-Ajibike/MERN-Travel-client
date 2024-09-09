42fdd2bf5298929ca1861ff6a113f2ac311ed8ae
Ambaaq-Ajibike <ajibikeambaaq@gmail.com>
2024-09-09 13:14:42 +0100

    pof design update

--- src/pages/ProofOfFund.jsx
+++ src/pages/ProofOfFund.jsx
@@ -3,8 +3,7 @@ const ProofOfFund = () => {
   return (
     <div className="bg-gray-100 pb-10">
       {/* First Section */}
-      <div className="bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${bgImg})`}}>
-        
+      <div className="relative bg-cover bg-center bg-no-repeat" style={{background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${bgImg})`}}>
         <div className="container mx-auto py-20 px-6 flex justify-between">
           <div className="max-w-xl">
             <h1 className="text-4xl font-bold text-white">
@@ -20,40 +19,43 @@ const ProofOfFund = () => {
             </button>
           </div>
 
-          <div className="bg-white p-6 rounded-md shadow-md max-w-sm ml-auto">
+          <div className="bg-gray-400 bg-opacity-50 p-6 rounded-md shadow-md max-w-sm ml-auto">
             <form>
-              <div className="mb-4">
-                <label
-                  htmlFor="amount"
-                  className="block text-gray-700 font-bold mb-2"
-                >
-                  How Much Do You Need?
-                </label>
-                <input
-                  type="text"
-                  id="amount"
-                  className="w-full px-3 py-2 border rounded-md"
-                  placeholder="e.g. 10m"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="duration"
-                  className="block text-gray-700 font-bold mb-2"
-                >
-                  How Long?
-                </label>
-                <input
-                  type="text"
-                  id="duration"
-                  className="w-full px-3 py-2 border rounded-md"
-                  placeholder="e.g. 30 days"
-                />
+            <h1  className="block text-white font-bold mb-2 text-xl"> How Much Do You Need?</h1>
+            <div className="flex space-x-4">
+                <div className="mb-4 w-1/2">
+                  <label
+                    htmlFor="amount"
+                    className="block text-white font-bold mb-2"
+                  >
+                    Amount
+                  </label>
+                  <input
+                    type="text"
+                    id="amount"
+                    className="w-full px-3 py-2 border rounded-md"
+                    placeholder="e.g: 10m"
+                  />
+                </div>
+                <div className="mb-4 w-1/2">
+                  <label
+                    htmlFor="howLong"
+                    className="block text-white font-bold mb-2"
+                  >
+                    How Long?
+                  </label>
+                  <input
+                    type="text"
+                    id="howLong"
+                    className="w-full px-3 py-2 border rounded-md"
+                    placeholder="eg: 30 days"
+                  />
+                </div>
               </div>
               <div className="mb-4">
                 <label
                   htmlFor="email"
-                  className="block text-gray-700 font-bold mb-2"
+                  className="block text-white font-bold mb-2"
                 >
                   Email Address
                 </label>
@@ -68,7 +70,7 @@ const ProofOfFund = () => {
                 <div className="mb-4 w-1/2">
                   <label
                     htmlFor="firstName"
-                    className="block text-gray-700 font-bold mb-2"
+                    className="block text-white font-bold mb-2"
                   >
                     First Name
                   </label>
@@ -82,7 +84,7 @@ const ProofOfFund = () => {
                 <div className="mb-4 w-1/2">
                   <label
                     htmlFor="lastName"
-                    className="block text-gray-700 font-bold mb-2"
+                    className="block text-white font-bold mb-2"
                   >
                     Last Name
                   </label>
@@ -137,11 +139,11 @@ const ProofOfFund = () => {
             },
           ].map((step, index) => (
             <div key={index} className="text-center max-w-xs flex items-center justify-between flex-col">
-              <div className="text-green-500 text-4xl font-bold mb-4 rounded-full w-20 h-20 border-green-700 border-solid border-2 p-5">
+              <div className="text-green-500 text-6xl font-bold mb-4 rounded-full w-24 h-24 border-green-700 border-solid border-2 flex justify-center items-center">
                 {index + 1}
               </div>
-              <h3 className="text-xl font-semibold mb-2">{step.title}</h3>
-              <p className="text-gray-600">{step.description}</p>
+              <h3 className="text-2xl font-bold mb-2">{step.title}</h3>
+              <p className="text-gray-600 text-lg">{step.description}</p>
             </div>
           ))}
         </div>
